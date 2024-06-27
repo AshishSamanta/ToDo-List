6e239d22b624d2aa05a14664faf39e763502a1ae
AshishSamanta <ashishsamanta758@gmail.com>
2024-06-27 16:33:48 +0530

    pushing it to repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import { useState } from 'react';
+import BTNmodule from './Button.module.css'
+import 'react-notifications/lib/notifications.css';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
 
 function App() {
+  let [todolist, settodolist] = useState([]);
+  let saveTodoList = (event) => {
+
+    let toName = event.target.toName.value;
+    // alert(toName);
+    if (!todolist.includes(toName)) {
+      let finalTodoList = [...todolist, toName];
+      settodolist(finalTodoList);
+    }
+    else {
+      alert("already added....")
+    }
+    event.preventDefault();
+  }
+  let list=todolist.map((value,item)=>{
+    return(
+      <TodoListItem value={value} key={item}  indexNumber={item}
+      todolist={todolist}
+      setTodoList={settodolist}
+      
+      />
+    )
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo LiSt</h1>
+      <form onSubmit={saveTodoList}>
+        <input type="text" name="toName"></input> <button>Save</button>
+      </form>
+      <div className='outerdiv'>
+        <ul>
+          {list}
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+function TodoListItem({value,indexNumber,todolist,setTodoList}){
+  let deleterow=()=>{
+    let finalData=todolist.filter((v,i)=>i!=indexNumber)
+    setTodoList(finalData)
+
+  }
+  return (
+    <li>{indexNumber+1}.    {value}<span onClick={deleterow}>&times;</span></li>
+  )
+}
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css"
+
+
+
+function Header(){
+    return(
+        <div>
+        Header code
+        </div>
+    )
+}
+
+export default Header
